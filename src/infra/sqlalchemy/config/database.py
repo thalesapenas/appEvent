632b46f2b702bf4abd1a5107413868c1273c0c10
thalesapenas <thales.rodrigues@ufpi.edu.br>
2024-05-19 15:22:50 -0300

    a

--- src/infra/sqlalchemy/config/database.py
+++ src/infra/sqlalchemy/config/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./EVENTS_database.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://events_zw1p_user:7KksyQBsQ5GcWbgbINGAd3SO7fpuWqd9@dpg-cp5426779t8c73eojch0-a.oregon-postgres.render.com/events_zw1p"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
